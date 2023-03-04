16ef9f7267cb9a3413267f10968dbac197ee78df
dev-rafa1707 <rafa1707@gmail.com>
2023-03-04 12:47:13 -0300

    Insere funcionalidade para calculo dos pacotes

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,62 @@
-// const toggleButton = document.getElementsByClassName('toggle-button')[0]
-// const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-// toggleButton.addEventListener('click', () => {
-//   navbarLinks.classList.toggle('active')
-// })
 
 
+function calculaPacoteDisney() {
+    let passagemAdulto = 1350
+    let passagemKids = 1050
+    let diaria = 530
+    var days = document.getElementById('days');
+    var daysNum = parseInt(days.value);
+    var adults = document.getElementById('adults');
+    var adultsNum = parseInt(adults.value);
+    var kids = document.getElementById('kids');
+    var kidsNum = parseInt(kids.value);
+    var valor = (daysNum * diaria) + (adultsNum * passagemAdulto) + (kidsNum * passagemKids);
+    var p = document.getElementById('resultado');
+    p.innerHTML = 'Valor do seu pacote R$: ' + valor
+}
 
 
+function calculaPacoteDubai() {
+    let passagemAdulto = 2100
+    let passagemKids = 1700
+    let diaria = 820
+    var days = document.getElementById('days');
+    var daysNum = parseInt(days.value);
+    var adults = document.getElementById('adults');
+    var adultsNum = parseInt(adults.value);
+    var kids = document.getElementById('kids');
+    var kidsNum = parseInt(kids.value);
+    var valor = (daysNum * diaria) + (adultsNum * passagemAdulto) + (kidsNum * passagemKids);
+    var p = document.getElementById('resultado');
+    p.innerHTML = 'Valor do seu pacote R$: ' + valor
+}
+
+function calculaPacoteFernando() {
+    let passagemAdulto = 950
+    let passagemKids = 720
+    let diaria = 350
+    var days = document.getElementById('days');
+    var daysNum = parseInt(days.value);
+    var adults = document.getElementById('adults');
+    var adultsNum = parseInt(adults.value);
+    var kids = document.getElementById('kids');
+    var kidsNum = parseInt(kids.value);
+    var valor = (daysNum * diaria) + (adultsNum * passagemAdulto) + (kidsNum * passagemKids);
+    var p = document.getElementById('resultado');
+    p.innerHTML = 'Valor do seu pacote R$: ' + valor
+}
+
+function calculaPacoteMachu() {
+    let passagemAdulto = 1100
+    let passagemKids = 875
+    let diaria = 450
+    var days = document.getElementById('days');
+    var daysNum = parseInt(days.value);
+    var adults = document.getElementById('adults');
+    var adultsNum = parseInt(adults.value);
+    var kids = document.getElementById('kids');
+    var kidsNum = parseInt(kids.value);
+    var valor = (daysNum * diaria) + (adultsNum * passagemAdulto) + (kidsNum * passagemKids);
+    var p = document.getElementById('resultado');
+    p.innerHTML = 'Valor do seu pacote R$: ' + valor
+}
